31ffda54b15fe24c75ac732f92ebc57c9c1f3c97
Juan Escossia Pagan <126635699+juanep23@users.noreply.github.com>
2023-06-12 08:02:08 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,82 @@
+//Questão 1
+function ehPrimo(number) {
+    if (number < 2) {
+      return false;
+    }
+  
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
+  
+  function verificarPrimo() {
+    var inputNumber = prompt('Digite um número:');
+    var number = parseInt(inputNumber);
+  
+    
+      if (ehPrimo(number)) {
+        alert(number + ' é um número primo.');
+      } else {
+        alert(number + ' não é um número primo.');
+      }
+  }
+
+  verificarPrimo();
+
+  //Questão 2
+  function validarSenha() {
+    const senha = prompt('Digite sua senha:');
+  
+    const possuiMaiuscula = /[A-Z]/.test(senha);
+    const possuiMinuscula = /[a-z]/.test(senha);
+    const possuiNumero = /\d/.test(senha);
+    const possuiTamanhoMinimo = senha.length >= 8;
+  
+    if (possuiMaiuscula && possuiMinuscula && possuiNumero && possuiTamanhoMinimo) {
+      alert('Parabéns sua senha é válida.');
+    } else {
+      alert('ERRO! Esta senha não é válida.');
+    }
+  }
+
+  validarSenha();
+
+  //Questão 3
+  function calcularFatorial(numero) {
+    if (numero === 0 || numero === 1) {
+      return 1;
+    } else {
+      let fatorial = 1;
+      for (let i = 2; i <= numero; i++) {
+        fatorial *= i;
+      }
+      return fatorial;
+    }
+  }
+  
+  function verificarFatorial() {
+    var numero = parseInt(prompt('Digite um número para calcular seu fatorial:'));
+  
+    var fatorial = calcularFatorial(numero);
+    alert(`O fatorial de ${numero} é ${fatorial}`);
+  }
+
+  verificarFatorial();
+
+  //Questão 4
+  function verificarQuadrado() {
+    var numero = prompt("Informe um número para verificar se é ou não um quadrado perfeito:");
+    var raizQuadrada = Math.sqrt(numero);
+    if (Math.floor(raizQuadrada) === raizQuadrada) {
+      alert("O número é um quadrado perfeito!");
+    } else {
+      alert("O número não é um quadrado perfeito.");
+    }
+  }
+
+  verificarQuadrado();
+  
